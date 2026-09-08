0ea69d9cb5594c771cab21a34f8dd740a96231a3
Joao Paulo <engs-joaocunico@camporeal.edu.br>
2026-09-08 20:03:51 -0300

    Atividade 6 - criando view principais

---
+++ resources/views/alunos/Index.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Lista Alunos</title>
+</head>
+<body>
+    <h1>Lista Alunos</h1>
+    <p>Alunos cadastrados:</p>
+
+    </ul>
+        <li>Joao</li>
+        <li>Mario</li>
+        <li>Pedro</li>
+    <ul>
+        
+
+</body>
+</html>
