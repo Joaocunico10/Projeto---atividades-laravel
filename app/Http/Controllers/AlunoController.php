3f09ba1141fd17a71c74ec738672a07f56b7c94d
Joao Paulo <engs-joaocunico@camporeal.edu.br>
2026-09-08 19:50:19 -0300

    Atividade 4 - Implementando 7 metodos CRUD

--- app/Http/Controllers/AlunoController.php
+++ app/Http/Controllers/AlunoController.php
@@ -6,5 +6,39 @@ use Illuminate\Http\Request;
 
 class AlunoController extends Controller
 {
-    //
+    public function index()
+    {
+        return 'Lista de alunos';
+    }
+
+    public function show($id)
+    {
+        return 'Aluno: ' . $id;
+    }
+
+    public function create()
+    {
+        return 'Formulário de cadastro';
+    }
+
+    public function store()
+    {
+        return 'Cadastrando aluno';
+    }
+
+    public function edit($id)
+    {
+        return 'Editando aluno';
+    }
+
+    public function update($id)
+    {
+        return 'Atualizando aluno';
+    }
+
+    public function destroy($id)
+    {
+        return 'Excluindo aluno';
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/sobre', function () {
+    return 'Página Sobre';
+});
+
+Route::get('/alunos', function () {
+    return 'Página de Alunos';
+});
+
+Route::get('/contato', function () {
+    return 'Página de Contato';
+});
+
+Route::get('/produto/{id}', function ($id) {
+    return 'Produto: ' . $id;
+});
+
+Route::get('/categoria/{id}', function ($id) {
+    return 'Categoria: ' . $id;
+});
+
+Route::get('/usuario/{id}', function ($id) {
+    return 'Usuário: ' . $id;
+});
