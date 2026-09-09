37b5650d445aefae0cc4d97338934e4a22ecf3ce
gustavo <engs-gustavoborges@camporeal.edu.br>
2026-09-09 19:28:27 -0300

    atv 21 - adicionando middlewere de adm

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/admin', function () {
+    return 'Area do administrador';
+})->middleware('admin');
+
 require __DIR__.'/auth.php';
