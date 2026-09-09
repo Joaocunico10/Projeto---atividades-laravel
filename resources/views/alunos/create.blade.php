9569bfdbb4875c4d75fc6153348e0ae2d71b58b5
Joao Paulo <engs-joaocunico@camporeal.edu.br>
2026-09-09 13:53:27 -0300

    atividade 14 - criando o formulario de cadastro

--- resources/views/alunos/create.blade.php
+++ resources/views/alunos/create.blade.php
@@ -4,4 +4,17 @@
 
     <h1>Cadastrar Aluno</h1>
 
+    <form action="/alunos" method="POST">
+        @csrf
+
+        <label>Nome:</label>
+        <input type="text" name="nome">
+
+        <label>Curso:</label>
+        <input type="text" name="curso">
+
+        <button type="submit">Cadastrar</button>
+
+    </form>
+
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@ Route::get('/alunos/curso', [AlunoController::class, 'porCurso']);
 Route::get('/alunos/nome', [AlunoController::class, 'porNome']);
 Route::get('/alunos/recentes', [AlunoController::class, 'recentes']);
 Route::get('/alunos/quantidade', [AlunoController::class, 'quantidade']);
-Route::get('/alunos/store', [AlunoController::class, 'store']);
+Route::get('/alunos/create', [AlunoController::class, 'create']);
 Route::get('/alunos/update/{id}', [AlunoController::class, 'update']);
 Route::get('/alunos/destroy/{id}', [AlunoController::class, 'destroy']);
+
+Route::post('/alunos', [AlunoController::class, 'store']);
