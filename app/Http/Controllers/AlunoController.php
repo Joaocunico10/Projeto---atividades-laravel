a3f53555dfbc0088d4dd202a7e232e09aa7e40bb
Joao Paulo <engs-joaocunico@camporeal.edu.br>
2026-09-08 21:42:54 -0300

    atv 11 - criando consultas

---
+++ app/Http/Controllers/AlunoController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Aluno;
+
+class AlunoController extends Controller
+{
+    public function index()
+    {
+        return 'Lista de alunos';
+    }
+
+    public function show($id)
+    {
+        return 'Aluno: ' . $id;
+    }
+
+    public function create()
+    {
+        return 'Formulário de cadastro';
+    }
+
+    public function store()
+    {
+        return 'Cadastrando aluno';
+    }
+
+    public function edit($id)
+    {
+        return 'Editando aluno';
+    }
+
+    public function update($id)
+    {
+        return 'Atualizando aluno';
+    }
+
+    public function destroy($id)
+    {
+        return 'Excluindo aluno';
+    }
+
+    public function porCurso()
+    {
+        return Aluno::where('curso', 'Engenharia de Software')->get();
+    }
+
+    public function porNome()
+    {
+        return Aluno::where('nome', 'LIKE', '%Joao%')->get();
+    }
+
+    public function recentes()
+    {
+        return Aluno::orderBy('created_at', 'desc')->get();
+    }
+
+    public function quantidade()
+    {
+        return Aluno::count();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AlunoController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/alunos/curso', [AlunoController::class, 'porCurso']);
+Route::get('/alunos/nome', [AlunoController::class, 'porNome']);
+Route::get('/alunos/recentes', [AlunoController::class, 'recentes']);
+Route::get('/alunos/quantidade', [AlunoController::class, 'quantidade']);
